e14fa01a848a2505ec88271c18c5e69e3e45fb68
Jeff Madsen <jrmadsen67@gmail.com>
2020-02-08 12:57:35 +0900

    file shell, just to set some things up

---
+++ src/FindDeadRoutes.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Routing\Route;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Input\InputOption;
+
+class FindDeadRoutes extends Command
+{
+}
